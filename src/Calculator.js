e9281fe555e3fce0106ad9a7140f0ff59c0f012e
Adam Frank White <adamfrankwhite@gmail.com>
2019-08-24 22:28:22 +0100

    init save

---
+++ src/Calculator.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Key from './Key'
+
+function Calculator(){
+    return (
+        <div>
+            <Key num="1" />
+            <Key num="2" />
+            <Key num="3" />
+            <Key num="4" />
+            <Key num="5" />
+            <Key num="6" />
+            <Key num="7" />
+            <Key num="8" />
+            <Key num="9" />
+            <Key num="=" />
+        </div>
+    )
+}
+
+export default Calculator
---
+++ src/Key.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Key(props) {
+    return (
+        <div>
+            <span>{props.num}</span>
+        </div>
+    )
+}
+
+export default Key
