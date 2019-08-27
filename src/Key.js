7278ebabc21d071c38ac476ffb59b5584515129f
Adam Frank White <adamfrankwhite@gmail.com>
2019-08-27 12:00:50 +0100

    added extra argument to clickHandler

--- src/Key.js
+++ src/Key.js
@@ -7,7 +7,7 @@ class Key extends React.Component {
         return (
                 <div 
                     className={`calc-key `} 
-                    onClick={() => this.props.clickHandler(this.props.num)} 
+                    onClick={() => this.props.clickHandler(this.props.val, this.props.num)} 
                 >
                     <span>{this.props.num}</span>
                 </div>
